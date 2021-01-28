14ee27bac8e7a601127feebd0b16435249717d05
CrimsonShadow26 <joel.m.gerber@gmail.com>
2021-01-28 01:13:39 -0600

    Simple player and camera controllers for testing collectable system and UI. Do not use these in the game.

---
+++ GlobalGameJam2021/Assets/Joel/Scripts/SimpleCameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleCameraController : MonoBehaviour
+{
+    [SerializeField] Transform target;
+
+    private Vector3 initialOffset;
+
+    private void Start()
+    {
+        initialOffset = target.position - transform.position;
+    }
+    void LateUpdate()
+    {
+        transform.position = target.position - initialOffset;
+    }
+}
---
+++ GlobalGameJam2021/Assets/Joel/Scripts/SimplePlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GGJ_Online
+{
+    public class SimplePlayerController : MonoBehaviour
+    {
+        [Header("This Component Is For Testing Purposes Only")]
+        [Header("Janky code and bad practices. DO NOT USE!")]
+
+        [SerializeField] float maxSpeed = 5.0f;
+        [SerializeField] float acceleration = 5.0f;
+
+        private Rigidbody rb;
+
+        private void Awake()
+        {
+            rb = GetComponent<Rigidbody>();
+        }
+
+        void Update()
+        {
+            Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical")) * maxSpeed;
+
+            Debug.Log(input);
+            rb.velocity = Vector3.MoveTowards(rb.velocity, input, acceleration * Time.deltaTime);
+            transform.LookAt(transform.position + input);
+        }
+    }
+}

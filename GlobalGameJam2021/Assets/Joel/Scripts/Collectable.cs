465317c298c4ed89ee969b074bfdefa15d6119a9
CrimsonShadow26 <joel.m.gerber@gmail.com>
2021-01-28 01:07:27 -0600

    Created collectable script for artifacts and rotator for whatever

---
+++ GlobalGameJam2021/Assets/Joel/Scripts/Collectable.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace GGJ_Online
+{
+    [RequireComponent(typeof(Collider))]
+    public class Collectable : MonoBehaviour
+    {
+        const int playerLayer = 8;
+
+        [Header("Must mark as trigger to Function")]
+        [SerializeField] float value;
+        [SerializeField] GameObject collectionEffect;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if(other.gameObject.layer == playerLayer)
+            {
+                ScoreManager.AddScore(value);
+
+                if (collectionEffect)
+                {
+                    GameObject.Instantiate(collectionEffect, transform.position, transform.rotation);
+                }
+
+                GameObject.Destroy(this.gameObject);
+            }
+        }
+    }
+}
---
+++ GlobalGameJam2021/Assets/Joel/Scripts/Rotator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    [Header("Euler Angel Rotation Speeds")]
+    [SerializeField] Vector3 degreesPerSecond;
+
+    [Header("Random Rotation Properties")]
+    [SerializeField] bool useRandomRotation;
+    [SerializeField] float minDegreesPerSecond;
+    [SerializeField] float maxDegreesPerSecond;
+
+    private void Awake()
+    {
+        if (useRandomRotation)
+        {
+            degreesPerSecond = new Vector3(GetRandomDPS(), GetRandomDPS(), GetRandomDPS());
+        }
+    }
+    void Update()
+    {
+        transform.Rotate(degreesPerSecond * Time.deltaTime);
+    }
+
+    private float GetRandomDPS()
+    {
+        return Random.Range(minDegreesPerSecond, maxDegreesPerSecond);
+    }
+}

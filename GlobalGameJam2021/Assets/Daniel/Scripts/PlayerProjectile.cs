80bb6504f796e202424793bc51934f5a3cf27d84
defisher <danielefisher3@gmail.com>
2021-01-29 21:40:48 -0600

    Training
    Added a temple room with an altar; pillars; and a fire; sfx are going; lightmap baked

--- GlobalGameJam2021/Assets/Daniel/Scripts/PlayerProjectile.cs
+++ GlobalGameJam2021/Assets/Daniel/Scripts/PlayerProjectile.cs
@@ -21,4 +21,11 @@ public class PlayerProjectile : MonoBehaviour
     {
         transform.Translate(Vector3.forward * projectileSpeed * Time.deltaTime);
     }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Wall")
+        {
+            Destroy(this.gameObject);
+        }
+    }
 }

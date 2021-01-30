015310416a7f919fee3b3cc39a177a3663b19815
CrimsonShadow26 <joel.m.gerber@gmail.com>
2021-01-29 21:30:08 -0600

    New spike trap

---
+++ GlobalGameJam2021/Assets/Joel/Scripts/SpearTrap.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GGJ_Online {
+    public class SpearTrap : MonoBehaviour
+    {
+        enum SpearState { Up, Down, Retracting, Extending };
+        [SerializeField] SpearState state = SpearState.Down;
+
+        [SerializeField] bool trapIsEnabled = false;
+        [SerializeField] float downTime = 5.0f;
+        [SerializeField] float upTime = 1.0f;
+        [SerializeField] float retractTime = 2.0f;
+        [SerializeField] float extendTime = 0.5f;
+        [SerializeField] int pulsesInDownState = 3;
+
+        [SerializeField] Transform spears;
+
+        const float upYPosition = 1.6f;
+        const float downYPosition = 0.0f;
+
+        float timeInState;
+
+        void Start()
+        {
+            timeInState = 0.0f;
+            TrapActivator activator = GetComponent<TrapActivator>();
+            if(activator)
+                activator.RegisterActivationFunction(EnableSpearTrap);
+        }
+
+        private void Update()
+        {
+            if (trapIsEnabled)
+                timeInState += Time.deltaTime;
+            else
+                timeInState = 0.0f;
+
+            if (state == SpearState.Down) DownState();
+            else if (state == SpearState.Extending) ExtendingState();
+            else if (state == SpearState.Up) UpState();
+            else if (state == SpearState.Retracting) RetractingState();
+        }
+
+        void ChangeState(SpearState newState)
+        {
+            timeInState = 0.0f;
+            state = newState;
+        }
+        void EnableSpearTrap()
+        {
+            ChangeState(SpearState.Down);
+        }
+
+        void DownState()
+        {
+            float t = NormalizedTime(downTime);
+            if (t >= 1.0f)
+                ChangeState(SpearState.Extending);
+
+            //Tweening func
+            t = -Mathf.Abs(Mathf.Cos(t * 10.0f * pulsesInDownState / Mathf.PI)) + 1.0f;
+            SetSpearPosition(t*0.2f);
+        }
+        void ExtendingState()
+        {
+            float t = NormalizedTime(extendTime);
+            if (t >= 1.0f)
+                ChangeState(SpearState.Up);
+
+            SetSpearPosition(t);
+
+        }
+        void UpState()
+        {
+            SetSpearPosition(1.0f);
+
+            if (NormalizedTime(upTime) >= 1.0f)
+                ChangeState(SpearState.Retracting);
+        }
+        void RetractingState()
+        {
+            float t = NormalizedTime(extendTime);
+            if (t >= 1.0f)
+                ChangeState(SpearState.Down);
+
+            SetSpearPosition(1-t);
+
+        }
+
+        float NormalizedTime(float fullStateTime)
+        {
+            return timeInState / fullStateTime;
+        }
+
+        void SetSpearPosition(float t)
+        {
+            spears.position = new Vector3(spears.position.x, Mathf.Lerp(downYPosition, upYPosition, t), spears.position.z);
+        }
+    }
+}
---
+++ GlobalGameJam2021/Assets/Joel/Scripts/TrapActivator.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GGJ_Online {
+    public class TrapActivator : MonoBehaviour
+    {
+        public delegate void ActivateTrap();
+
+        [Header("Score Activation")]
+        [SerializeField] bool useSocreActivation;
+        [SerializeField] float scoreToActivate;
+        [Header("Time Activation")]
+        [SerializeField] bool useTimeActivation;
+        [Tooltip("Time in seconds")]
+        [SerializeField] float timeToActivate;
+
+        ActivateTrap activate;
+
+        private void Awake()
+        {
+            activate = new ActivateTrap(TurnOnTrap);
+        }
+        void Start()
+        {
+            if(useSocreActivation)
+                ScoreManager.RegisterScoreChangeEvent(ScoreChanged);
+
+            if (useTimeActivation)
+                Invoke("Activate", timeToActivate);
+        }
+
+        void ScoreChanged(float totalScore, float pointsAdded)
+        {
+            if (totalScore > scoreToActivate)
+                Activate();
+        }
+
+        void TurnOnTrap()
+        {
+            #if UNITY_EDITOR
+            Debug.Log("Trap activated: " + name);
+            #endif
+        }
+
+        void Activate()
+        {
+            activate();
+        }
+
+        public void RegisterActivationFunction(ActivateTrap function)
+        {
+            activate += function;
+        }
+    }
+}

d3359db390c24b6d1996c86319d10fbe64274f42
CrimsonShadow26 <joel.m.gerber@gmail.com>
2021-01-31 15:07:21 -0600

    Sanity saving code additon

---
+++ GlobalGameJam2021/Assets/Joel/Scripts/LockMouseToGameWindow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockMouseToGameWindow : MonoBehaviour
+{
+    static LockMouseToGameWindow instance;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            DestroyImmediate(this.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+#if UNITY_EDITOR
+// Nothing
+#else
+        Cursor.lockState = CursorLockMode.Confined;
+#endif
+    }
+}
